e448625ffcc60f7af0a2e4479c8696b0f227efcf
Varalakshmi-chikkam <105534008+Varalakshmi-chikkam@users.noreply.github.com>
2024-06-28 18:33:19 +0530

    first commit

--- mystore/admin.py
+++ mystore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .product import Product
+from .category import Category
 # Register your models here.
+
+class Categoryinfo(admin.ModelAdmin):
+    list_display=["name"]
+admin.site.register(Product)
+admin.site.register(Category, Categoryinfo)
